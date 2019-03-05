95e8fb599a845a32e4cd16fefe5e8bf396a89940
Ben Shpetner <bshpetner00@stuy.edu>
2019-03-04 20:50:30 -0500

    edited toString to display underscores in case of blank space

--- KnightBoard.java
+++ KnightBoard.java
@@ -21,13 +21,28 @@ public class KnightBoard {
 		for (int i = 0; i < board.length; < i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (board[i][j] % 10 == board[i][j]) {
-					s = s + "   " + board[i][j];
+					if (board[i][j] == 0) {
+						s = s + "   _"; 
+					}
+					else {
+						s = s + "   " + board[i][j];
+					}
 				}
 				else if (board[i][j] % 100 == board[i][j]) {
-					s = s + "  " + board[i][j];
+					if (board[i][j] == 0) {
+						s = s + "  _"; 
+					}
+					else {
+						s = s + "  " + board[i][j];
+					}
 				}
 				else {
-					s = s + " " + board[i][j];
+					if (board[i][j] == 0) {
+						s = s + " _"; 
+					}
+					else {
+						s = s + " " + board[i][j];
+					}
 				}
 			}
 			s += "\n";
