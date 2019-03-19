ab5caa5f90f491c4cb19d07c70d88e1aa31228f1
Ben Shpetner <bshpetner00@stuy.edu>
2019-03-19 17:49:29 -0400

    committing old changes

--- Driver.java
+++ Driver.java
@@ -1,5 +1,69 @@
 public class Driver {
+	//testcase must be a valid index of your input/output array
+public static void runTest(int i){
+
+  KnightBoard b;
+  int[]m =   {4,5,5,5,5};
+  int[]n =   {4,5,4,5,5};
+  int[]startx = {0,0,0,1,2};
+  int[]starty = {0,0,0,1,2};
+  int[]answers = {0,304,32,56,64};
+  if(i >= 0 ){
+    try{
+      int correct = answers[i];
+      b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+      int ans  = b.countSolutions(startx[i],starty[i]);
+
+      if(correct==ans){
+        System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+      }else{
+        System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+      }
+    }catch(Exception e){
+      System.out.println("FAIL Exception case: "+i);
+
+    }
+  }
+}
 	public static void main(String[] args) {
-		
+		/*for (int i = 0; i < 5; i++) {
+			runTest(i);
+		}*/
+		String time;
+
+		System.out.println("SOLVE: (from (0, 0))\n");
+		for (int r = 1; r < 7; r++) {
+			KnightBoard b = new KnightBoard(r, r);
+			long startTime = System.nanoTime();
+			try {
+				if (r == 51 || r == 52)
+					time = "skipped";
+				else if (b.solve(0, 0))
+					time = Math.round((System.nanoTime()-startTime)/10000.)/100.+"ms";
+				else
+					time = "unsolvable";
+				System.out.printf("\t%dx%d:\t%s%n", r, r, time);
+			} catch (StackOverflowError e) {
+				System.out.printf("\t%dx%d:\tstack overflow%n", r, r);
+				break;
+			}
+		}
+
+		System.out.println("\nCOUNT SOLUTIONS: (from (0, 0))\n");
+		for (int r = 1; r < 6; r++) {
+			for (int c = r; c < 6; c++) {
+				KnightBoard b = new KnightBoard(r, c);
+				long startTime = System.nanoTime();
+				try {
+					int n = b.countSolutions(0, 0);
+					time = Math.round((System.nanoTime()-startTime)/10000.)/100.+"ms";
+					System.out.printf("\t%dx%d:\t%d solutions\t\t%s%n", r, c, n, time);
+				} catch (StackOverflowError e) {
+					System.out.printf("\t%dx%d:\tstack overflow%n", r, c);
+					break;
+				}
+			}
+		}
 	}
 }
--- KnightBoard.java
+++ KnightBoard.java
@@ -90,7 +90,45 @@ public class KnightBoard {
 			}
 		}
 		return false;
-	}	
+	}
+
+	public int countSolutions(int firstRow, int firstCol) throws IllegalStateException,IllegalArgumentException {
+		if (firstRow < 0 || firstCol < 0) {
+			throw new IllegalArgumentException("Invalid board index");
+		}
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				if (board[i][j] != 0) {
+					throw new IllegalStateException("Invalid board state. Clear it or suffer.");
+				}
+			}
+		}
+		return countHelp(firstRow, firstCol, 1);
+	}
+
+	public int countHelp(int r, int c, int knights) {
+		int arr = 0;
+		int see = 0;
+		int count = 0;
+		if (r >= board.length || c >= board[0].length || r < 0 || c < 0) {
+			return 0;
+		}
+		else if (board[r][c] != 0){
+			return 0;
+		}
+		else if (knights == board.length * board[r].length) {
+			board[r][c] = knights;
+			return 1;
+		}
+			for (int[]move: moves) {
+				board[r][c] = knights;
+				arr = r + move[0];
+				see = c + move[1];
+				count += countHelp(arr,see,knights+1);
+				board[r][c] = 0;
+			}
+		return count;
+	}
 
 	public static void main(String[] args) {
 		KnightBoard b = new KnightBoard(6,6);
